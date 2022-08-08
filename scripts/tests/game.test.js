f9545c1141cd3c563ac7e8fd090d1490aa0f2fe4
Steph Hjartarson <stephanie.hja@gmail.com>
2022-08-08 19:34:26 +0000

    Create and test addTurn function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -41,11 +41,11 @@ describe("newGame works correctly", () => {
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("should clear the playerMoves array", () => {
-        expect(game.playerMoves.length).toBe(0);
+    test("should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
-    test("should clear the currentGame array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
     });
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
